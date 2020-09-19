132da8157d68b36ef3c99a0086ceb6b2437cba2a
brunodesde1987 <bruno@solutweb.com.br>
2020-09-19 15:23:22 -0300

    Add routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,39 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Home from '../pages/Home';
 
 const Routes = () => {
   return (
     <Switch>
-      <Route path="/" exact component={Dashboard} />
+      <Route path="/" exact component={Home} />
+      <Route
+        path="/administracao-e-financas"
+        exact
+        render={() => <Dashboard teamName={'Administração e Finanças'} />}
+      />
+      <Route
+        path="/gente-e-gestao"
+        exact
+        render={() => <Dashboard teamName={'Gente e Gestão'} />}
+      />
+      <Route
+        path="/novos-negocios-e-mobilizacao-de-recursos"
+        exact
+        render={() => (
+          <Dashboard teamName={'Novos Negócios e Mobilização de Recursos'} />
+        )}
+      />
+      <Route
+        path="/operacoes"
+        exact
+        render={() => <Dashboard teamName={'Operações'} />}
+      />
+      <Route
+        path="/tecnologia-da-informacao"
+        exact
+        render={() => <Dashboard teamName={'Tecnologia da Informação'} />}
+      />
     </Switch>
   );
 };
